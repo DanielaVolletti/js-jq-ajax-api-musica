9d51ac23461e67ceba764381133f0bf686f9ebdc
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-04-20 18:25:25 +0200

    bonus con creazione select per i generi musicali

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
           "cover": singoloAlbum.poster,
           "title": singoloAlbum.title,
           "artist": singoloAlbum.author,
-          "year": singoloAlbum.year
+          "year": singoloAlbum.year,
+          "genre": singoloAlbum.genre
         };
         var html = template(context);
         // inserisco i dischi in html
@@ -30,6 +31,24 @@ $(document).ready(function () {
     error: function(richiesta,stato,errore){
       alert("Chiamata fallita!!!");
     }
+  });
+
+  // cambio genere con on change, facendo vedere solo il genere selezionato
+  $(".filtro-genere select").on("change", function(){
+    var selezioneGenere = $(this).val().toLowerCase();
+    if(selezioneGenere === ""){
+      $('.container').show();
+    } else {
+      $(".container").each(function(){
+        var genereCd = $(this).data("genre").toLowerCase();
+        if (genereCd === selezioneGenere){
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      })
+    }
+
   });
 
 
